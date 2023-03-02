e3cf1518bd025d3f50ff6389ea7d8b21b7b60990
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2023-03-02 09:58:56 +0200

    Add files via upload

---
+++ KT.php
@@ -0,0 +1,226 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Peedu Erik Pajo IT-21</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <title>Peedu Erik Pajo IT21</title>
+    <style>
+    .jumbo{
+            
+            background-color: #666666;
+            background-image: url('https://4kwallpapers.com/images/walls/thumbs_3t/5656.jpg');
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #fff;
+            height: 623px;
+            width: 100%;
+        }
+        .jumbo1{
+            
+            background-color: #666666;
+            background-image: url('https://orstedcdn.azureedge.net/-/media/www/images/corp/investor/cmdbanner-v2.ashx?mh=1440&mw=1440&rev=b67f4d2c418144769ee6847569434b5b&hash=247E67AA7D9C9C839C59C870D53C6C05');
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #fff;
+            height: 648px;
+            width: 100%;
+        }
+        .jumbo2{
+            background-image: url('https://www.solidbackgrounds.com/images/2560x1440/2560x1440-davys-grey-solid-color-background.jpg');
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #fff;
+            height: 100%;
+            width: 100%;
+        }
+        </style>
+</head>
+  <body>
+
+          <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="KT.php">Peedu Erik Pajo</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="KT.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="KT1.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="KT2.php">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Templates</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                </ul>
+                    <i class="fa-brands fa-instagram " style="margin-right: 15px;"></i>
+                    <i class="fa-brands fa-twitter" style="margin-right: 15px;"></i>
+                    <i class="fa-brands fa-linkedin" style="margin-right: 15px;"></i>
+                <form class="d-flex" role="search">
+                  <a href="login.php">Logi Sisse</a>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+
+
+
+
+    <div class="row jumbo">
+        
+        <div class="col p-2  text-center">
+            <h1 class="mt-5">Everything you need to craft a beautiful</h1>
+            <h1>business websites</h1>
+            <button class="btn btn-outline-light mt-5" type="submit">Get started now</button>
+            <a class="btn btn-light text-primary mt-5" type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank">Watch video</a>
+            <p class="mt-3">No Creadit Card Required!</p>
+            <div>
+                <img src="https://picsum.photos/150/60" style="margin-right: 100px;" width="150" height="60">
+                <img src="https://picsum.photos/150/60" style="margin-right: 100px;" width="150" height="60">
+                <img src="https://picsum.photos/150/60" style="margin-right: 100px;" width="150" height="60">
+                <img src="https://picsum.photos/150/60" style="margin-right: 100px;" width="150" height="60">
+                <img src="https://picsum.photos/150/60" style="margin-right: 100px;" width="150" height="60">
+                <img src="https://picsum.photos/150/60"  width="150" height="60">
+            </div>
+            <div class="container">
+            <h1 class="mt-5" >HERE'S WHAT YOU GET</h1>
+            <p class="mt-2 text-center" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nesciunt iusto illum ullam cumque, officia iste soluta assumenda qui! Sapiente praesentium officiis aliquam quibusdam dicta nemo ut velit deserunt error!</p>
+            </div>
+            <a class="btn btn-light text-primary mt-5" type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank"><i class="fa-solid fa-laptop"></i>  Responsive layouts  </a>
+            <a class="btn btn-light text-primary mt-5" type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank"><i class="fa-regular fa-gem"></i>Modern design</a>
+            <a class="btn btn-light text-primary mt-5" type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank"><i class="fa-brands fa-bootstrap"></i>Bootstrap 4</a>
+            <a class="btn btn-primary text-light mt-5" type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank"><i class="fa-brands fa-html5"></i>Clean HTML5 code</a>
+        </div>
+    </div>
+<div class="row p-5 jumbo1">
+  <div class="col">
+  <h1 class="text-dark mt-5 ">Clean and Valid HTML5 code</h1>
+          <p class="text-dark" style="width:600px;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas corrupti dolor ipsa. Earum eius laboriosam explicabo illo laudantium qui alias saepe quasi veniam provident recusandae, ut molestiae inventore similique deserunt.</p>
+          <div class="card" style="width:600px;">
+            <div class="card-body" >
+              <div class="row">
+                <div class="col-3">
+                  
+                  <img src="https://picsum.photos/120/80" class="rounded mx-auto d-block">
+                  <p class="text-center text-dark " >-- John Doe</p>
+                  <p class="text-center text-dark ">Anton OÜ</p>
+                </div>
+                <div class="col-6">
+                  
+                    <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit.Quas corrupti dolor ipsa. Earum eius laboriosam explicabo illo laudantium qui alias saepe quasi</p>
+                    <i class="fa-solid fa-quote-right text-dark fa-2x"></i>
+                  
+                </div>
+              </div>
+            </div>
+        </div>
+          <a class="btn btn-primary text-light mt-5 "  type="submit" href="https://youtu.be/lJkOaBXU8lM" target="_blank">See all features</a>
+  </div>
+  <div class="col ">
+  <img src="https://picsum.photos/600/600" class="mx-auto d-block">
+  </div>
+</div>
+<section class="pt-5 pb-5 jumbo2">
+  <div class="footer ">
+    <div class="     pt-5 pb-5">
+        <div class="container">
+            <div class="row  ">
+                <div class="col-xs-6 col-sm-3">
+                    <a href="#">
+                        <svg class="d-block mb-4" width="36" height="36" viewBox="0 0 612 612" xmlns="http://www.w3.org/2000/svg" focusable="false">
+                            <title>Bootstrap</title>
+                            <path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"></path>
+                            <path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"></path>
+                        </svg>
+                    </a>
+                    <address class="color-light-20">
+                  1600 Amphitheatre Parkway<br>
+                  Mountain View, CA 94043
+                </address>
+                    <ul class="list-unstyled list-light">
+                        <li><a href="#">Disclaimer</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-xs-6 col-sm-3">
+                    <h4 class="my-2">Product</h4>
+                    <ul class="list-unstyled list-light text-white">
+                        <li><a class=" " href="#">Pricing</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Customers</a></li>
+                        <li><a href="#">Store</a></li>
+                    </ul>
+                </div>
+                <br style="clear:both" class="hidden-sm-up">
+                <div class="col-xs-6 col-sm-3">
+                    <h4 class="my-2">Company</h4>
+                    <ul class="list-unstyled list-light">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Press</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-sm-3">
+                    <h4 class="my-2">Connect</h4>
+                    <ul class="list-unstyled list-light">
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Social</a></li>
+                        <li><a href="#">Community</a></li>
+                    </ul>
+                    <p class="color-light mt-2">Stay up-to-date!</p>
+                    <form>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control form-control-sm" placeholder="Email" aria-label="Email" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary btn-sm form-control-sm" type="button">OK</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr>
+            <div class="row f-flex justify-content-between" style="justify-content: space-between;">
+                <div class="col-md-8 text-xs-center  text-left   my-1">
+                    <p class="mt-2  text-muted"> © Copyright 2019 • All Rights Reserved | <a class=" " href="#">Disclaimer</a> | <a class=" " href="#">Contact</a></p>
+                </div>
+                <div class="col-md-4 text-xs-center   text-lg-right   my-1">
+                    <div class="btn-container  mt-1 text-md-right text-sm-center">
+                        <div class="mb-1 mr-3 align-self-right pt-0 d-inline-block">
+                            <a href="#" role="button" class=" btn-white p-2 m-2 btn btn-round btn-rised btn-icon x"><i class="fab fa-twitter fa-lg color-light" aria-hidden="true"></i></a>
+                            <a href="#" role="button" class="btn-white p-2 m-2 btn btn-round btn-rised btn-icon "><i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>
+                            <a href="#" role="button" class="btn-white p-2 m-2 btn btn-round btn-rised btn-icon  "><i class="fab fa-linkedin fa-lg" aria-hidden="true"></i></a>
+                            <a href="#" role="button" class="btn-white p-2 m-2 btn btn-rised btn-round btn-icon  "><i class="fab fa-google-plus fa-lg" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+   </body>
+</html>
